56b26fd11bc3b5cc2f3399d87cce0655b9a37cb2
Vinicius Gomes <vinicius.svgomes@gmail.com>
2022-12-07 10:58:01 -0300

    stuff

--- src/solvers/bnb.py
+++ src/solvers/bnb.py
@@ -6,33 +6,27 @@ from math import ceil
 
 import sys
 
-def init_bound(weights):
+def initial_bound(weights, no_nodes):
+    total = 0
+
+    sorted_weights = weights.copy()
+    sorted_weights.sort(axis=1)
+
+    for i in range(no_nodes):
+        total += sorted_weights[i][1]
+        total += sorted_weights[i][2]
+
+    return ceil(total / 2)
     total = 0
     no_nodes = weights.shape[0]
 
-    sorted = weights.copy().sort(axis=1)
-
-    for i in range(no_nodes):
-        total += weights[i][1]
-        total += weights[i][2]
-
-    return ceil(total / 2)
-
-def bound(weights, solution):
-    if len(solution) == 0: init_bound(weights)
-    else:
-        total = 0
-        no_nodes = weights.shape[0]
-
-
-
 def bnb_tsp(weights, graph):
-    root = (bound(weights, []), 0, 0, [0]) # bound, level, cost, solution
+    no_nodes = graph.number_of_nodes()
+
+    root = (initial_bound(weights, no_nodes), 0, 0, [0]) # bound, level, cost, solution
     queue = PriorityQueue()
     best = sys.maxsize # infinity
     solution = [] # vertices order
-
-    no_nodes = graph.number_of_nodes()
 
     queue.put(root)
 
@@ -45,29 +39,25 @@ def bnb_tsp(weights, graph):
                 solution = partial_solution
         elif partial_bound < best:
             connect_to_source = weights[partial_solution[-1][0]]
-            best_bound = bound(partial_solution.copy().append(0))
+            best_bound = bound(weights, partial_solution.copy().append(0), no_nodes)
 
             if node.level < no_nodes:
                 for k in range(no_nodes):
                     distance = weights[partial_solution[-1]][k]
-                    bound_value = bound(partial_solution.copy().append(k))
+                    bound_value = bound(weights, partial_solution.copy().append(k), no_nodes)
 
-                    if k not in partial_solution and
-                    distance != 0 and
-                    bound_value < best:
-                        new_node = (bound_value, level + 1,
-                                    cost + distance, partial_solution.copy().append(k))
+                    if k not in partial_solution and distance != 0 and bound_value < best:
+                        new_node = (bound_value,
+                                    level + 1,
+                                    cost + distance,
+                                    partial_solution.copy().append(k))
                         queue.put(new_node)
-            elif connect_to_source != 0 and
-            best_bound < best and
-            len(node.solution) == no_nodes:
-                possible_solution = ()
-                queue.put(best_bound,
-                          level + 1,
-                          cost + connect_to_source,
-                          partial_solution.copy().append(0))
 
-
-    print(solution, best)
+            elif connect_to_source != 0 and best_bound < best and len(node.solution) == no_nodes:
+                possible_solution = (best_bound,
+                                     level + 1,
+                                     cost + connect_to_source,
+                                     partial_solution.copy().append(0))
+                queue.put(possible_solution)
 
     return solution, best
--- src/utils/book.py
+++ src/utils/book.py
@@ -1,10 +1,17 @@
 # example of the book (to verify if the algorithms implemented are correct)
+import os
+import sys
+
+util_dir = os.path.dirname(__file__)
+solvers = os.path.join(util_dir, '..')
+sys.path.append(solvers)
 
 import numpy as np
 import networkx as nx
 
 from solvers.tat import tat_tsp
 from solvers.christofides import christofides_tsp
+from solvers.bnb import bnb_tsp
 
 entrie = np.array([[ 0, 4,  8,  9, 12],
                    [ 4, 0,  6,  8,  9],
@@ -12,14 +19,27 @@ entrie = np.array([[ 0, 4,  8,  9, 12],
                    [ 9, 8, 10,  0,  7],
                    [12, 9, 11,  7,  0]])
 
-graph = nx.from_numpy_matrix(entrie, create_using=nx.Graph())
+e_bnb = np.array([[0, 3, 1, 5, 8],
+                [3, 0, 6, 7, 9],
+                [1, 6, 0, 4, 2],
+                [5, 7, 4, 0, 3],
+                [8, 9, 2, 3, 0]])
 
-tat_sol, tat_cost = tat_tsp(graph)
-print("TAT:")
-print("Path",tat_sol)
-print("Cost:", tat_cost)
+bnb_graph = nx.from_numpy_matrix(entrie, create_using=nx.Graph())
 
-ch_sol, ch_cost = christofides_tsp(graph)
-print("CHRISTOFIDES:")
-print("Path:", ch_sol)
-print("Cost:", ch_cost)
+sol, cost = bnb_tsp(e_bnb, bnb_graph)
+
+print(sol)
+print(cost)
+
+#graph = nx.from_numpy_matrix(entrie, create_using=nx.Graph())
+
+#tat_sol, tat_cost = tat_tsp(graph)
+#print("TAT:")
+#print("Path",tat_sol)
+#print("Cost:", tat_cost)
+
+#ch_sol, ch_cost = christofides_tsp(graph)
+#print("CHRISTOFIDES:")
+#print("Path:", ch_sol)
+#print("Cost:", ch_cost)

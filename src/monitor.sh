931295618fec365fe7842e53d8d6b37df8dad323
Vinicius Gomes <vinicius.svgomes@gmail.com>
2022-12-09 20:48:43 -0300

    update bash script

--- src/monitor.sh
+++ src/monitor.sh
@@ -8,7 +8,15 @@ for i in {4..10}; do
 
     for algo in "${algorithms[@]}"; do
         for distance in "${distances[@]}"; do
-            timeout 10s echo "hello $i" || [ $? -eq 124 ] && echo timeouted
+            timeout 10s ping google.com
+            EXIT_STATUS=$?
+
+            if [ $EXIT_STATUS -eq 124 ]
+            then
+                echo "Entrie: 2^$i-$distance - Algorithm: $algo - failed!"
+            else
+                echo "Entrie: 2^$i-$distance - Algorithm: $algo - success!"
+            fi
         done
     done
 done
